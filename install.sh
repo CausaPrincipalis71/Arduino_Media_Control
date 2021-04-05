c37a7e4802358d9b9532e01d71541ab6d2451e55
MasterTinka <znamenochick@gmail.com>
2021-04-05 19:19:34 +0600

    Исправлен установщик

--- install.sh
+++ install.sh
@@ -12,14 +12,6 @@ make
 mv ./Arduino_Media_Control /bin/Arduino_Media_Control
 cd /bin
 
-echo -e "#!/bin/bash\n$(pwd)/Arduino_Media_control" > Arduino_Media_Control.sh
-
-chmod +x Arduino_Media_Control.sh
-
-cd /etc/systemd/system 
-
-echo -e "[Unit]\nDescription = Mouse Control\n\n[Service]\nRemainAfterExit=true\nExecStart=/bin/Arduino_Media_Control.sh\nType=oneshot\n\n[Install]\nWantedBy=multi-user.target\n" > Arduino_Media_Control.service
-
-systemctl enable Arduino_Media_Control.service
+echo -e "[Desktop Entry]\nName=Arduino_Media_Control\nNoDisplay=true\nType=Application\nTerminal=false\nExec=Arduino_Media_Control" > /etc/xdg/autostart/Arduino_Media_Control
 
 echo "*****INSTALLATION DONE*****"
